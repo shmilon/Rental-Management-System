bfc5a9012358a9610072266b85425b6da7c50936
Sazid Hasan Milon <shams007.bd@gmail.com>
2023-06-08 09:20:21 +0600

    Rental Management System with PHP and Javascript with MySQL Database

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+                
+    <!-- <script type="text/javascript" src="./assets/scripts/main.js"></script> -->
+    <script type="text/javascript" src="./assets/scripts/main.js"></script>
+    <script type="text/javascript" src="./assets/scripts/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="./assets/scripts/sweetalert2.all.min.js"></script>
+    <!-- <script type="text/javascript" src="./assets/scripts/sweetalert2.all.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body> -->
+
+</html>
